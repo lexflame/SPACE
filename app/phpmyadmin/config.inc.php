1e3c37fe62a128e1e5c2eefadd07bd0b654cd5f3
lexflame <lexflame@gmail.com>
2025-07-14 20:37:09 +0700

    New SPACE, task and begin maps
    New SPACE, task and begin maps

---
+++ app/phpmyadmin/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$i=0;
+$i++;
+$cfg['Servers'][$i]['user']          = 'root';
+$cfg['Servers'][$i]['password']      = 'qwerty123'; // use here your password
+$cfg['Servers'][$i]['auth_type']     = 'config';
+$cfg['Servers'][$i]['host'] 		 = '127.0.0.1';
